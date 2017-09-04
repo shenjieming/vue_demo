36e6f504af2b8cf8501a2e294f7c46142f4d9831
shenjieming <495100766@qq.com>
2017-09-04 14:52:30 +0800

    配置环境文件提交

---
+++ buildCfg/production/baseCfg.js
@@ -0,0 +1,4 @@
+const baseCfg={
+  httpUrl:'http://scm.med-log.com.cn/oms-api-v3'
+};
+export default baseCfg;
---
+++ src/buildCfg/baseCfg.js
@@ -0,0 +1,4 @@
+const baseCfg={
+  httpUrl:'http://192.168.1.135/oms-api-v3'
+};
+export default baseCfg;
